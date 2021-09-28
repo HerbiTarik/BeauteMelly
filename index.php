589dbfb1b76af65f10c62d9d02ba92ef3f136f77
Herbi Tarik <herbitarik2509@gmail.com>
2021-09-28 14:30:20 +0200

    version1

--- index.php
+++ index.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/c79ef9f9bb.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" type="text/javascript" href="js/script.js">
-    <style>
-        @import url('https://fonts.googleapis.com/css2?family=Monsieur+La+Doulaise&display=swap');
-        </style>
-      
-    <title>Beauté Melly</title>
-</head>
+<?php include("header_footer/head.php");?>
 <body>
 
     <!--header-->

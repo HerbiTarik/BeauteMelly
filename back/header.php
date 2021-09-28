b3049bffb59434b0c4b7f9dd5c8db91bf930aa1d
Herbi Tarik <herbitarik2509@gmail.com>
2021-09-28 14:49:32 +0200

    version3

---
+++ back/header.php
@@ -0,0 +1,35 @@
+    <!--header-->
+
+    <header>
+    
+        <div class="logo"><a href="#">BeautéMelly</a></div>
+       
+        <input type="checkbox" id="chk">
+        <label for="chk" class="show-menu-btn">
+            <i class="fas fa-bars"></i>
+        </label>
+
+        <ul class="menu">
+            
+            <li id="left"><a href="#"><i class="fas fa-shopping-bag"></i><strong> Produits</strong></a></li>
+            <li id="left"><a href="#"><i class="far fa-gem"></i><strong> Marques</strong></a></li>
+            <li id="left"><a href="#"><i class="far fa-newspaper"></i><strong> Magazine</strong></a></li>
+            <li id="left"><a href="#"><i class="fas fa-shopping-cart"></i><strong> Panier</strong></a></li>
+            <label for="chk" class="hide-menu-btn">
+                <i class="fas fa-times"></i>
+            </label>
+            
+        </ul>
+        <ul class="profile">
+            <li><a href="connexion.php"><i class="fas fa-user"></i><strong> S'identifier</strong></a></li>
+        </ul>
+    </header>
+
+    <nav class="navbarre">
+                <div class="divsearch">
+                <div class="search">
+                     <input class="search-txt" type="text" name="box" placeholder="Rechercher un produit...">
+                     <a  class="search-btn"href="#"><i class="fas fa-search"></i></a>
+                </div>
+            </div>
+           </nav>

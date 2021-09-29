d87b458c700515b730768330882fda77115a41bc
Herbi Tarik <herbitarik2509@gmail.com>
2021-09-29 22:17:49 +0200

    version16

--- back/footer.php
+++ back/footer.php
@@ -3,40 +3,40 @@
             <div class="container">
                 <div class="row">
                     <div class="footer-col">
-                        <h4>company</h4>
+                        <h4>entreprise</h4>
                         <ul>
-                            <li><a href="#">about us</a></li>
-                            <li><a href="#">our services</a></li>
-                            <li><a href="#">privacy policy</a></li>
-                            <li><a href="#">affiliate program</a></li>
+                            <li><a href="#">à propos de nous</a></li>
+                            <li><a href="#">nos services</a></li>
+                            <li><a href="#">politique de confidentialité</a></li>
+                            <li><a href="#">Programme d'affiliation</a></li>
                         </ul>
                     </div>
                     <div class="footer-col">
-                        <h4>get help</h4>
+                        <h4>Obtenir de l'aide</h4>
                         <ul>
                             <li><a href="#">FAQ</a></li>
-                            <li><a href="#">shipping</a></li>
-                            <li><a href="#">returns</a></li>
-                            <li><a href="#">order status</a></li>
-                            <li><a href="#">payment options</a></li>
+                            <li><a href="#">Expédition</a></li>
+                            <li><a href="#">Retour</a></li>
+                            <li><a href="#">Statut de la commande</a></li>
+                            <li><a href="#">Options de paiement</a></li>
                         </ul>
                     </div>
                     <div class="footer-col">
-                        <h4>online shop</h4>
+                        <h4>Magasin en ligne</h4>
                         <ul>
-                            <li><a href="#">watch</a></li>
-                            <li><a href="#">bag</a></li>
-                            <li><a href="#">shoes</a></li>
-                            <li><a href="#">dress</a></li>
+                            <li><a href="#">acceuil</a></li>
+                            <li><a href="#">produit</a></li>
+                            <li><a href="#">marque</a></li>
+                            <li><a href="#">magazine</a></li>
                         </ul>
                     </div>
                     <div class="footer-col">
-                        <h4>follow us</h4>
+                        <h4>Suivez-nous</h4>
                         <div class="social-links">
-                            <a href="#"><i class="fab fa-facebook-f"></i></a>
-                            <a href="#"><i class="fab fa-twitter"></i></a>
-                            <a href="#"><i class="fab fa-instagram"></i></a>
-                            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                            <a href="www.facebook.com"><i class="fab fa-facebook-f"></i></a>
+                            <a href="www.twitter.com"><i class="fab fa-twitter"></i></a>
+                            <a href="www.instagram.com"><i class="fab fa-instagram"></i></a>
+                            <a href="www.linkedin.com"><i class="fab fa-linkedin-in"></i></a>
                         </div>
                     </div>
                 </div>
